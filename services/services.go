4b203a3736b37a936150deb9508e91bd32bc0de9
vishalpatidar99 <vishupatidar10@gmail.com>
2024-07-09 12:40:11 +0530

    feat: added validation in services and update accordingly

--- services/services.go
+++ services/services.go
@@ -2,7 +2,7 @@ package services
 
 import (
 	"context"
-	"fmt"
+	"log"
 
 	"github.com/vishalpatidar99/Go-grpc-user-service/models"
 	pb "github.com/vishalpatidar99/Go-grpc-user-service/protos/compiled"
@@ -19,13 +19,18 @@ type server struct {
 func Server() *server {
 	return &server{
 		users: []models.User{
-			{ID: 1, FName: "Steve", City: "LA", Phone: 1234567890, Height: 5.8, Married: true},
-			{ID: 2, FName: "John", City: "NY", Phone: 9876543210, Height: 6.1, Married: false},
+			{ID: 1, FName: "Steve", City: "LA", Phone: 1234567890, Height: 5.8, Married: "true"},
+			{ID: 2, FName: "John", City: "NY", Phone: 9876543210, Height: 6.1, Married: "false"},
 		},
 	}
 }
+
 func (s *server) GetUserByID(ctx context.Context, req *pb.UserIDRequest) (*pb.UserResponse, error) {
-	fmt.Println("Reached at service ------------")
+	log.Print("Get user by id service invoked, proceeding with request")
+	if err := utils.GetUserByIDValidation(req); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+
 	for _, u := range s.users {
 		if u.ID == req.Id {
 			return &pb.UserResponse{User: &pb.User{
@@ -42,6 +47,11 @@ func (s *server) GetUserByID(ctx context.Context, req *pb.UserIDRequest) (*pb.Us
 }
 
 func (s *server) GetUsersByIDs(ctx context.Context, req *pb.UserIDsRequest) (*pb.UsersResponse, error) {
+	log.Print("Get users by list of id service invoked, proceeding with request")
+	if err := utils.GetUsersByIDsValidation(req); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+
 	var users []*pb.User
 	for _, id := range req.Ids {
 		for _, u := range s.users {
@@ -64,12 +74,18 @@ func (s *server) GetUsersByIDs(ctx context.Context, req *pb.UserIDsRequest) (*pb
 }
 
 func (s *server) SearchUsers(ctx context.Context, req *pb.SearchRequest) (*pb.UsersResponse, error) {
+	log.Print("Search users service invoked, proceeding with request")
+	if err := utils.SearchUsersValidation(req); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+
 	var users []*pb.User
-	utils.SearchUsersValidation(req)
 	for _, u := range s.users {
-		if (req.City != "" || req.City == u.City) &&
-			(req.Phone == 0 || u.Phone == req.Phone) &&
-			(req.Married == u.Married) {
+		if u.City == req.City ||
+			u.FName == req.Fname ||
+			u.Phone == req.Phone ||
+			u.Height == req.Height ||
+			u.Married == req.Married {
 			users = append(users, &pb.User{
 				Id:      u.ID,
 				Fname:   u.FName,
